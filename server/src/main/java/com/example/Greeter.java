7fad96d79b5159ce6e65d58bd51c93e8c7de3743
mrcsptr <36894757+mrcsptr@users.noreply.github.com>
2019-03-05 15:30:48 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,8 @@
 package com.example;
 
+/**
+ * This is a class.
+ */
 
 public class Greeter {
 
@@ -7,7 +10,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+ * @param someone name of a person
+ * @return greeting string
+ */
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
